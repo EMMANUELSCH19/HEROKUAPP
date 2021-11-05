22b09be61365be92cacaf91ac419664c7cacae45
EMMANUELSCH19 <emmanuel_1320114041@uptecamac.edu.mx>
2021-11-05 14:18:45 -0600

    a

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba PHP</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
 </head>
+
 <body>
-
-    <?php
-        echo "Hola Mundo";
-    ?>
-
+    <div class="container">
+        <div class="text">
+            <p>¿Quieres ser mi novia?</p>
+        </div>
+        <div class="buttons">
+            <button class="btn-si" id="si">SI</button>
+            <button class="btn-no" id="no">NO</button>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="main.css"></script>
 </body>
+
 </html>

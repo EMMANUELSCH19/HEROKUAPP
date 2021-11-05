02e9b7a34f4f55b291fc343b0d649d2318ce2c53
EMMANUELSCH19 <emmanuel_1320114041@uptecamac.edu.mx>
2021-11-05 14:22:44 -0600

    a

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="main.css"></script>
+    <script src="main.js"></script>
 </body>
 
 </html>
